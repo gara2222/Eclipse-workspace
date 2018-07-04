64980272e5331b226bd05b33500a86044491a51f
keisuke hayashi <keisukehayashi@Mackenzie.local>
2018-07-04 21:29:12 +0900

    ok2

--- Capsule/src/Capsule.java
+++ Capsule/src/Capsule.java
@@ -5,8 +5,10 @@ public class Capsule {
 		System.out.println(taro.getName()); //privateの場合、Personクラス内のgetNameをつけること(taro.name → taro.getName())
 		System.out.println(taro.getAge()); //privateの場合、Personクラス内のgetAgeをつけること(taro.age → taro.getAge())
 		
-		//setNameを使う
+		//setNameを使った変数の置き換え
 		taro.setName("花子");
 		System.out.println(taro.getName());
+		taro.setAge(18);
+		System.out.println(taro.getAge());
 	}
 }
